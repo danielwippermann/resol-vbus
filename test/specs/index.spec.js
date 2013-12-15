ea55129d3b1573575478075d57bd208ecfda8baf
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2013-12-15 09:28:02 +0100

    - Added library entry test spec

---
+++ test/specs/index.spec.js
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * resol-vbus - A JavaScript library for processing RESOL VBus data
+ */
+'use strict';
+
+
+
+var index = require('./resol-vbus');
+
+
+
+describe('RESOL VBus', function() {
+
+    it('should be an object', function() {
+        expect(index).to.be.an('object');
+    });
+
+});
