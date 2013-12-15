25dffcbf4771eaa01c97dd0c0b79254ebdc14b60
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2013-12-15 09:26:08 +0100

    - Added missing library and test entry files

---
+++ src/index.js
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * resol-vbus - A JavaScript library for processing RESOL VBus data
+ */
+'use strict';
+
+
+
+var extend = require('./extend');
+
+var Specification = require('./specification');
+
+// var Header = require('./header');
+// var Packet = require('./packet');
+// var Datagram = require('./datagram');
+// var Telegram = require('./telegram');
+
+// var Connection = require('./connection');
+
+// var TcpDataSourceProvider = require('./tcp-data-source-provider');
+// var TcpConnection = require('./tcp-connection');
+
+
+
+module.exports = {
+    extend: extend,
+    
+    Specification: Specification,
+
+    // Header: Header,
+    // Packet: Packet,
+    // Datagram: Datagram,
+    // Telegram: Telegram,
+    
+    // Connection: Connection,
+    
+    // TcpDataSourceProvider: TcpDataSourceProvider,
+    // TcpConnection: TcpConnection,
+
+};
---
+++ test/specs/resol-vbus.js
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * resol-vbus - A JavaScript library for processing RESOL VBus data
+ */
+'use strict';
+
+
+
+var vbus = require('../../src/index');
+
+
+
+module.exports = vbus;
