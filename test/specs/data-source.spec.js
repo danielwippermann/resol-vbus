275516f02a33f614ab7f360637debedb0d1de505
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2013-12-26 20:29:02 +0100

    - Added tests for data source

---
+++ test/specs/data-source.spec.js
@@ -0,0 +1,105 @@
+/*! resol-vbus | Copyright (c) 2013, Daniel Wippermann | MIT license */
+'use strict';
+
+
+
+var DataSource = require('./resol-vbus').DataSource;
+
+
+
+describe('DataSource', function() {
+
+    describe('constructor', function() {
+
+        it('should be a constructor function', function() {
+            expect(DataSource).to.be.a('function');
+        });
+
+        it('should have reasonable defaults', function() {
+            var dataSource = new DataSource();
+
+            expect(dataSource).to.be.an('object');
+            expect(dataSource.provider).to.equal(null);
+            expect(dataSource.id).to.equal(null);
+            expect(dataSource.name).to.equal(null);
+            expect(dataSource.description).to.equal(null);
+            expect(dataSource.isSupportingLiveData).to.equal(false);
+            expect(dataSource.isSupportingRecordedData).to.equal(false);
+        });
+
+        it('should copy selected options', function() {
+            var dataSource = new DataSource({
+                provider: 'provider',
+                id: 'id',
+                name: 'name',
+                description: 'description',
+                isSupportingLiveData: true,
+                isSupportingRecordedData: true,
+                junk: 'JUNK',
+            });
+
+            expect(dataSource).to.be.an('object');
+            expect(dataSource.provider).to.equal('provider');
+            expect(dataSource.id).to.equal('id');
+            expect(dataSource.name).to.equal('name');
+            expect(dataSource.description).to.equal('description');
+            expect(dataSource.isSupportingLiveData).to.equal(true);
+            expect(dataSource.isSupportingRecordedData).to.equal(true);
+            expect(dataSource.junk).to.equal(undefined);
+        });
+
+    });
+
+    describe('#connectLive', function() {
+
+        it('should be a method', function() {
+            expect(DataSource.prototype.connectLive).to.be.a('function');
+        });
+
+        it('should report if not supported', function() {
+            var converter = new DataSource();
+
+            expect(function() {
+                converter.connectLive();
+            }).to.throw(Error, 'Does not support live data');
+        });
+
+        it('should be abstract', function() {
+            var converter = new DataSource({
+                isSupportingLiveData: true,
+            });
+
+            expect(function() {
+                converter.connectLive();
+            }).to.throw(Error, 'Must be implemented by sub-class');
+        });
+
+    });
+
+    describe('#openRecorder', function() {
+
+        it('should be a method', function() {
+            expect(DataSource.prototype.openRecorder).to.be.a('function');
+        });
+
+        it('should report if not supported', function() {
+            var converter = new DataSource();
+
+            expect(function() {
+                converter.openRecorder();
+            }).to.throw(Error, 'Does not support recorded data');
+        });
+
+        it('should be abstract', function() {
+            var converter = new DataSource({
+                isSupportingRecordedData: true,
+            });
+
+            expect(function() {
+                converter.openRecorder();
+            }).to.throw(Error, 'Must be implemented by sub-class');
+        });
+
+    });
+
+});
