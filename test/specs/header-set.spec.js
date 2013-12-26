399a9b612891b3ad012c6216659633a3f403cbfd
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2013-12-26 21:58:13 +0100

    - Extended tests for VBus header set

--- test/specs/header-set.spec.js
+++ test/specs/header-set.spec.js
@@ -10,82 +10,266 @@ var HeaderSet = require('./resol-vbus').HeaderSet;
 
 describe('HeaderSet', function() {
 
-    it('should be a constructor function', function() {
-        expect(HeaderSet).to.be.a('function');
-        expect(HeaderSet.extend).to.be.a('function');
+    describe('constructor', function() {
+
+        it('should be a constructor function', function() {
+            expect(HeaderSet).to.be.a('function');
+            expect(HeaderSet.extend).to.be.a('function');
+        });
+
+        it('should have reasonable defaults', function() {
+            var before = new Date();
+            var headerSet = new HeaderSet();
+            var after = new Date();
+
+            expect(headerSet).to.be.an('object');
+            expect(headerSet.timestamp).to.be.an.instanceOf(Date);
+            expect(headerSet.timestamp.getTime()).to.be.within(before.getTime(), after.getTime());
+        });
+
+        it('should copy certain options', function() {
+            var header1 = new Packet({
+                channel: 1
+            });
+
+            var header2 = new Packet({
+                channel: 2
+            });
+
+            var options = {
+                timestamp: new Date(),
+                headers: [ header1, header2 ]
+            };
+
+            var headerSet = new HeaderSet(options);
+
+            expect(headerSet).to.be.an('object');
+            expect(headerSet.timestamp).to.equal(options.timestamp);
+            expect(headerSet.headerList).to.eql(options.headers);
+        });
+
     });
 
-    it('should have reasonable defaults', function() {
-        var before = new Date();
-        var headerSet = new HeaderSet();
-        var after = new Date();
+    describe('#addHeader', function() {
 
-        expect(headerSet).to.be.an('object');
-        expect(headerSet.timestamp).to.be.an.instanceOf(Date);
-        expect(headerSet.timestamp.getTime()).to.be.within(before.getTime(), after.getTime());
+        it('should be a method', function() {
+            expect(HeaderSet.prototype.addHeader).to.be.a('function');
+        });
+
+        it('should work correctly', function() {
+            var header1 = new Packet({
+                channel: 1
+            });
+
+            var header2 = new Packet({
+                channel: 2
+            });
+
+            var header3 = new Packet({
+                channel: 2
+            });
+
+            var headerSet = new HeaderSet();
+
+            headerSet.addHeader(header1);
+
+            expect(headerSet.headerList).to.eql([ header1 ]);
+
+            headerSet.addHeader(header2);
+
+            expect(headerSet.headerList).to.eql([ header1, header2 ]);
+
+            headerSet.addHeader(header3);
+
+            expect(headerSet.headerList).to.eql([ header1, header3 ]);
+        });
+
     });
 
-    it('should copy certain options', function() {
-        var header1 = new Packet({
-            channel: 1
+    describe('#addHeaders', function() {
+
+        it('should be a method', function() {
+            expect(HeaderSet.prototype.addHeaders).to.be.a('function');
         });
 
-        var header2 = new Packet({
-            channel: 2
+        it('should work correctly', function() {
+            var header1 = new Packet({
+                channel: 1
+            });
+
+            var header2 = new Packet({
+                channel: 2
+            });
+
+            var header3 = new Packet({
+                channel: 2
+            });
+
+            var headerSet = new HeaderSet();
+
+            headerSet.addHeaders([ header1, header2, header3 ]);
+
+            expect(headerSet.headerList).to.eql([ header1, header3 ]);
         });
 
-        var options = {
-            timestamp: new Date(),
-            headers: [ header1, header2 ]
-        };
-
-        var headerSet = new HeaderSet(options);
-
-        expect(headerSet).to.be.an('object');
-        expect(headerSet.timestamp).to.equal(options.timestamp);
     });
 
-    it('should have a getId method', function() {
-        var header1 = new Packet({
-            channel: 1
+    describe('#removeAllHeaders', function() {
+
+        it('should be a method', function() {
+            expect(HeaderSet.prototype.removeAllHeaders).to.be.a('function');
         });
 
-        var header2 = new Packet({
-            channel: 2
+        it('should work correctly', function() {
+            var header1 = new Packet({
+                channel: 1
+            });
+
+            var header2 = new Packet({
+                channel: 2
+            });
+
+            var headerSet = new HeaderSet({
+                headers: [ header1, header2 ],
+            });
+
+            headerSet.removeAllHeaders();
+
+            expect(headerSet.headerList).to.eql([]);
         });
 
-        var options = {
-            timestamp: new Date(),
-            headers: [ header1, header2 ]
-        };
-
-        var headerSet = new HeaderSet(options);
-
-        expect(headerSet).to.be.an('object');
-        expect(headerSet.getId).to.be.a('function');
-        expect(headerSet.getId()).to.equal('01_0000_0000_10_0000,02_0000_0000_10_0000');
     });
 
-    it('should have a getIdHash method', function() {
-        var header1 = new Packet({
-            channel: 1
+    describe('#removeHeadersOlderThan', function() {
+
+        it('should be a method', function() {
+            expect(HeaderSet.prototype.removeHeadersOlderThan).to.be.a('function');
         });
 
-        var header2 = new Packet({
-            channel: 2
+        it('should work correctly', function() {
+            var header1 = new Packet({
+                timestamp: new Date(1388089665000),
+                channel: 1
+            });
+
+            var header2 = new Packet({
+                timestamp: new Date(1388089666000),
+                channel: 2
+            });
+
+            var headerSet = new HeaderSet({
+                headers: [ header1, header2 ],
+            });
+
+            headerSet.removeHeadersOlderThan(new Date(1388089665500));
+
+            expect(headerSet.headerList).to.eql([ header2 ]);
         });
 
-        var options = {
-            timestamp: new Date(),
-            headers: [ header1, header2 ]
-        };
-
-        var headerSet = new HeaderSet(options);
-
-        expect(headerSet).to.be.an('object');
-        expect(headerSet.getIdHash).to.be.a('function');
-        expect(headerSet.getIdHash()).to.equal('5c9c71b01aca96a35c15cffd0ec382e8a1be99b3e42eeff57ecd7836aa7f1a24');
     });
 
+    describe('#getHeaderCount', function() {
+
+        it('should be a method', function() {
+            expect(HeaderSet.prototype.getHeaderCount).to.be.a('function');
+        });
+
+        it('should work correctly', function() {
+            var header1 = new Packet({
+                channel: 1
+            });
+
+            var header2 = new Packet({
+                channel: 2
+            });
+
+            var header3 = new Packet({
+                channel: 2
+            });
+
+            var headerSet = new HeaderSet({
+                headers: [ header1, header2, header3 ]
+            });
+
+            expect(headerSet.getHeaderCount()).to.eql(2);
+        });
+
+    });
+
+    describe('#getSortedHeaders', function() {
+
+        it('should be a method', function() {
+            expect(HeaderSet.prototype.getSortedHeaders).to.be.a('function');
+        });
+
+        it('should work correctly', function() {
+            var header1 = new Packet({
+                channel: 1
+            });
+
+            var header2 = new Packet({
+                channel: 2
+            });
+
+            var headerSet = new HeaderSet({
+                headers: [ header2, header1 ]
+            });
+
+            expect(headerSet.getSortedHeaders()).to.eql([ header1, header2 ]);
+        });
+
+    });
+
+    describe('#getId', function() {
+
+        it('should be a method', function() {
+            expect(HeaderSet.prototype.getId).to.be.a('function');
+        });
+
+        it('should work correctly', function() {
+            var header1 = new Packet({
+                channel: 1
+            });
+
+            var header2 = new Packet({
+                channel: 2
+            });
+
+            var headerSet = new HeaderSet({
+                headers: [ header2, header1 ]
+            });
+
+            var id = headerSet.getId();
+
+            expect(id).to.equal('01_0000_0000_10_0000,02_0000_0000_10_0000');
+        });
+
+    });
+
+    describe('#getIdHash', function() {
+
+        it('should be a method', function() {
+            expect(HeaderSet.prototype.getIdHash).to.be.a('function');
+        });
+
+        it('should work correctly', function() {
+            var header1 = new Packet({
+                channel: 1
+            });
+
+            var header2 = new Packet({
+                channel: 2
+            });
+
+            var headerSet = new HeaderSet({
+                headers: [ header2, header1 ]
+            });
+
+            var id = headerSet.getIdHash();
+
+            expect(id).to.equal('5c9c71b01aca96a35c15cffd0ec382e8a1be99b3e42eeff57ecd7836aa7f1a24');
+        });
+
+    });
 
 });
