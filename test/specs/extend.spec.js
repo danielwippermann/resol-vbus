4b53637c2cec062b480b01cde22597b1d5e39d72
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2013-12-26 21:00:50 +0100

    - Extended tests for the extend helper

--- test/specs/extend.spec.js
+++ test/specs/extend.spec.js
@@ -57,6 +57,31 @@ describe('extend', function() {
         expect(instance).to.be.an.instanceOf(ChildClass);
     });
 
+    it('should work without parent class but with a constructor', function() {
+        var childClassConstructor = sinon.spy();
+
+        var ChildClass = testExtend(null, {
+
+            protoProp1: true,
+
+            constructor: childClassConstructor,
+
+        }, {
+
+            staticProp1: true
+
+        });
+
+        expect(ChildClass.staticProp1).to.equal(true);
+        expect(ChildClass.prototype.protoProp1).to.equal(true);
+
+        var instance = new ChildClass();
+
+        expect(instance).to.be.an('object');
+        expect(instance).to.be.an.instanceOf(ChildClass);
+        expect(childClassConstructor.callCount).to.equal(1);
+    });
+
     it('should work with parent class', function() {
         var ParentClass = testExtend(null, {
 
@@ -87,6 +112,50 @@ describe('extend', function() {
 
         expect(instance).to.be.an('object');
         expect(instance).to.be.an.instanceOf(ChildClass);
+    });
+
+    it('should work with parent class and constructors', function() {
+        var parentClassConstructor = sinon.spy();
+
+        var ParentClass = testExtend(null, {
+
+            protoProp1: true,
+
+            constructor: parentClassConstructor,
+
+        }, {
+
+            staticProp1: true
+
+        });
+
+        var childClassConstructor = sinon.spy(function() {
+            ParentClass.call(this);
+        });
+        
+        var ChildClass = testExtend(ParentClass, {
+
+            protoProp2: true,
+
+            constructor: childClassConstructor,
+
+        }, {
+
+            staticProp2: true
+
+        });
+
+        expect(ChildClass.staticProp1).to.equal(true);
+        expect(ChildClass.staticProp2).to.equal(true);
+        expect(ChildClass.prototype.protoProp1).to.equal(true);
+        expect(ChildClass.prototype.protoProp2).to.equal(true);
+
+        var instance = new ChildClass();
+
+        expect(instance).to.be.an('object');
+        expect(instance).to.be.an.instanceOf(ChildClass);
+        expect(childClassConstructor.callCount).to.equal(1);
+        expect(parentClassConstructor.callCount).to.equal(1);
     });
 
 });
