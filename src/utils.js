5bb65f779b9cdd2631add9a13e31d8807bc5c7ba
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2014-01-19 13:34:04 +0100

    - Added utils module

---
+++ src/utils.js
@@ -0,0 +1,75 @@
+/*! resol-vbus | Copyright (c) 2013-2014, Daniel Wippermann | MIT license */
+'use strict';
+
+
+
+var Q = require('q');
+
+
+
+var utils = {
+
+    /**
+     * @see http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+     */
+    generateGUID: function() {
+        var s4 = function() {
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        };
+
+        var guid = [
+            s4(),
+            s4(),
+            '-',
+            s4(),
+            '-',
+            s4(),
+            '-',
+            s4(),
+            '-',
+            s4(),
+            s4(),
+            s4(),
+        ].join('');
+
+        return guid;
+    },
+
+    promise: function(callback, thisArg) {
+        var deferred = Q.defer();
+        var promise = deferred.promise;
+
+        var done = function(err, result) {
+            if (deferred) {
+                if (err) {
+                    deferred.reject(err);
+                } else {
+                    deferred.resolve(result);
+                }
+
+                deferred = null;
+            }
+        };
+
+        var resolve = function(result) {
+            done(null, result);
+        };
+
+        var reject = function(reason) {
+            done(reason);
+        };
+
+        try {
+            callback.call(thisArg, resolve, reject);
+        } catch(ex) {
+            done(ex);
+        }
+
+        return promise;
+    },
+
+};
+
+
+
+module.exports = utils;
