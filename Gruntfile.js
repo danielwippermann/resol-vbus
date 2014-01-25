f3378ba6eb26d53b81939c2136428558fc9f56b8
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2014-01-25 10:15:53 +0100

    JSDoc output now uses DocStrap template

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,7 @@ module.exports = function(grunt) {
                 ],
                 options: {
                     destination: 'docs',
+                    template: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
                     configure: 'jsdoc.json'
                 }
             }
