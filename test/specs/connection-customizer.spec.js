e1c2ad88e743811b6a02b617c04223d2a6affabc
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2014-02-16 07:52:18 +0100

    Complete ConnectionCustomizer tests.

--- test/specs/connection-customizer.spec.js
+++ test/specs/connection-customizer.spec.js
@@ -12,6 +12,7 @@ var ConfigurationOptimizer = vbus.ConfigurationOptimizer;
 var Connection = vbus.Connection;
 var ConnectionCustomizer = vbus.ConnectionCustomizer;
 var Datagram = vbus.Datagram;
+var Packet = vbus.Packet;
 
 
 
@@ -95,6 +96,94 @@ describe('ConnectionCustomizer', function() {
                     .that.is.equal(options.masterTimeout);
         });
 
+    });
+
+    describe('#transceiveConfiguration', function() {
+
+        it('should be a method', function() {
+            expect(ConnectionCustomizer.prototype)
+                .to.have.a.property('transceiveConfiguration')
+                    .that.is.a('function');
+        });
+
+        it('should get values correctly', function(done) {
+            var deviceAddress = 0x1111;
+            var valueId = 0x2222;
+            var value = 0x12345678;
+
+            var connection = new Connection();
+
+            connection.pipe(connection);
+
+            var request, response;
+            connection.on('datagram', function(datagram) {
+                if (datagram.command === 0x0600) {
+                    var packet = new Packet({
+                        channel: 0,
+                        destinationAddress: 0x0010,
+                        sourceAddress: deviceAddress,
+                        command: 0x0100,
+                        frameCount: 0,
+                    });
+
+                    connection.send(packet);
+                } else if (datagram.destinationAddress === deviceAddress) {
+                    request = datagram;
+
+                    response = new Datagram({
+                        channel: request.channel,
+                        destinationAddress: request.sourceAddress,
+                        sourceAddress: deviceAddress,
+                        command: 0x0100,
+                        valueId: request.valueId,
+                        value: value,
+                    });
+
+                    connection.send(response);
+                }
+            });
+
+            connection._setConnectionState(Connection.STATE_CONNECTED);
+
+            var customizer = new ConnectionCustomizer({
+                deviceAddress: deviceAddress,
+                connection: connection,
+            });
+
+            var options = {
+                action: 'get',
+            };
+
+            var callback = sinon.spy(function(config, round) {
+                if (round === 1) {
+                    config = [
+                        { valueIndex: 0x1234, pending: true },
+                    ];
+                }
+                return config;
+            });
+
+            setTimeout(function() {
+                var datagram = new Datagram({
+                    channel: 0,
+                    destinationAddress: 0,
+                    sourceAddress: deviceAddress,
+                    command: 0x0500,
+                    valueId: 0,
+                    value: 0,
+                });
+
+                connection.send(datagram);
+            }, 10);
+
+            testUtils.performAsyncTest(done, function() {
+                return customizer.transceiveConfiguration(options, callback).then(function(config) {
+                    expect(config)
+                        .to.be.an('array')
+                        .that.has.lengthOf(1);
+                });
+            });
+        });
     });
 
     describe('#transceiveValue', function() {
@@ -221,6 +310,174 @@ describe('ConnectionCustomizer', function() {
                     expect(request)
                         .to.have.a.property('value')
                             .that.is.equal(value);
+
+                    expect(response).to.be.instanceOf(Datagram);
+                    expect(response.getId())
+                        .to.equal('00_0020_1111_20_0100_0000');
+                    expect(response)
+                        .to.have.a.property('valueId')
+                            .that.is.equal(valueId);
+                    expect(response)
+                        .to.have.a.property('value')
+                            .that.is.equal(value);
+                });
+            });
+        });
+
+        it('should contact the master regularly', function(done) {
+            var deviceAddress = 0x1111;
+            var valueId = 0x2222;
+            var value = 0x12345678;
+
+            var connection = new Connection();
+
+            connection.pipe(connection);
+
+            var request, response;
+            connection.on('datagram', function(datagram) {
+                if (datagram.destinationAddress === deviceAddress) {
+                    request = datagram;
+
+                    response = new Datagram({
+                        channel: request.channel,
+                        destinationAddress: request.sourceAddress,
+                        sourceAddress: deviceAddress,
+                        command: 0x0100,
+                        valueId: request.valueId,
+                        value: value,
+                    });
+
+                    connection.send(response);
+                }
+            });
+
+            connection._setConnectionState(Connection.STATE_CONNECTED);
+
+            var customizer = new ConnectionCustomizer({
+                deviceAddress: deviceAddress,
+                connection: connection,
+            });
+
+            var options = {
+                action: 'get',
+            };
+
+            var state = {
+                masterAddress: 0x4444,
+                masterLastContacted: Date.now() - 10000,
+            };
+
+            testUtils.performAsyncTest(done, function() {
+                return customizer.transceiveValue(valueId, 0, options, state).then(function(datagram) {
+                    expect(datagram.toLiveBuffer()).to.eql(response.toLiveBuffer());
+
+                    expect(request).to.be.instanceOf(Datagram);
+                    expect(request.getId())
+                        .to.equal('00_1111_0020_20_0300_0000');
+                    expect(request)
+                        .to.have.a.property('valueId')
+                            .that.is.equal(valueId);
+                    expect(request)
+                        .to.have.a.property('value')
+                            .that.is.equal(0);
+
+                    expect(response).to.be.instanceOf(Datagram);
+                    expect(response.getId())
+                        .to.equal('00_0020_1111_20_0100_0000');
+                    expect(response)
+                        .to.have.a.property('valueId')
+                            .that.is.equal(valueId);
+                    expect(response)
+                        .to.have.a.property('value')
+                            .that.is.equal(value);
+                });
+            });
+        });
+
+        it('should release and claim the VBus on timeout', function(done) {
+            var deviceAddress = 0x1111;
+            var valueId = 0x2222;
+            var value = 0x12345678;
+
+            var connection = new Connection();
+
+            connection.pipe(connection);
+
+            var request, response, reclaimed;
+            connection.on('datagram', function(datagram) {
+                if (datagram.command === 0x0600) {
+                    reclaimed = true;
+
+                    var packet = new Packet({
+                        channel: 0,
+                        destinationAddress: 0x0010,
+                        sourceAddress: deviceAddress,
+                        command: 0x0100,
+                        frameCount: 0,
+                    });
+
+                    connection.send(packet);
+
+                    setTimeout(function() {
+                        var busOffer = new Datagram({
+                            channel: 0,
+                            destinationAddress: 0x0000,
+                            sourceAddress: deviceAddress,
+                            command: 0x0500,
+                            valueId: 0,
+                            value: 0,
+                        });
+
+                        connection.send(busOffer);
+                    }, 10);
+                } else if (datagram.destinationAddress === deviceAddress) {
+                    if (!reclaimed) {
+                        // nop
+                    } else {
+                        request = datagram;
+
+                        response = new Datagram({
+                            channel: request.channel,
+                            destinationAddress: request.sourceAddress,
+                            sourceAddress: deviceAddress,
+                            command: 0x0100,
+                            valueId: request.valueId,
+                            value: value,
+                        });
+
+                        connection.send(response);
+                    }
+                }
+            });
+
+            connection._setConnectionState(Connection.STATE_CONNECTED);
+
+            var customizer = new ConnectionCustomizer({
+                deviceAddress: deviceAddress,
+                connection: connection,
+            });
+
+            var options = {
+                action: 'get',
+                actionOptions: {
+                    tries: 1,
+                    timeout: 100,
+                }
+            };
+
+            testUtils.performAsyncTest(done, function() {
+                return customizer.transceiveValue(valueId, 0, options).then(function(datagram) {
+                    expect(datagram.toLiveBuffer()).to.eql(response.toLiveBuffer());
+
+                    expect(request).to.be.instanceOf(Datagram);
+                    expect(request.getId())
+                        .to.equal('00_1111_0020_20_0300_0000');
+                    expect(request)
+                        .to.have.a.property('valueId')
+                            .that.is.equal(valueId);
+                    expect(request)
+                        .to.have.a.property('value')
+                            .that.is.equal(0);
 
                     expect(response).to.be.instanceOf(Datagram);
                     expect(response.getId())
