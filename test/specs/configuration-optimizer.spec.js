2ddaff073dde4ee59bb670a541c4787f0f246b03
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2014-03-19 04:30:02 +0100

    Add preliminary ConfigurationOptimizer test.

---
+++ test/specs/configuration-optimizer.spec.js
@@ -0,0 +1,48 @@
+/*! resol-vbus | Copyright (c) 2013-2014, Daniel Wippermann | MIT license */
+'use strict';
+
+
+
+var vbus = require('./resol-vbus');
+
+
+
+var ConfigurationOptimizer = vbus.ConfigurationOptimizer;
+
+
+
+describe('ConfigurationOptimizer', function() {
+
+    describe('constructor', function() {
+
+        it('should be a constructor function', function() {
+            expect(ConfigurationOptimizer).a('function');
+        });
+
+        it('should copy the device address', function() {
+            var deviceAddress = 0x1234;
+
+            var optimizer = new ConfigurationOptimizer(deviceAddress);
+
+            expect(optimizer).property('deviceAddress').equal(deviceAddress);
+        });
+
+    });
+
+    describe('getInitialConfiguration', function() {
+
+        it('should be a method', function() {
+            expect(ConfigurationOptimizer.prototype).property('getInitialConfiguration').a('function');
+        });
+
+    });
+
+    describe('optimizeConfiguration', function() {
+
+        it('should be a method', function() {
+            expect(ConfigurationOptimizer.prototype).property('optimizeConfiguration').a('function');
+        });
+
+    });
+
+});
