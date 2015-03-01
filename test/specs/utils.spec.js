e601a6a9b4734bb737ec01a650927b876c511c47
Daniel Wippermann <daniel.wippermann@gmail.com>
2015-03-01 01:31:43 +0100

    Add tests for number rounding helper function.

--- test/specs/utils.spec.js
+++ test/specs/utils.spec.js
@@ -113,4 +113,26 @@ describe('utils', function() {
 
     });
 
+    describe('roundNumber', function() {
+
+        it('should be a function', function() {
+            expect(utils.roundNumber).to.be.a('function');
+        });
+
+        it('should work correctly', function() {
+            var result = utils.roundNumber(55.55, -1);
+            expect(result).to.equal(55.6);
+
+            result = utils.roundNumber(55.549, -1);
+            expect(result).to.equal(55.5);
+
+            result = utils.roundNumber(1.005, -2);
+            expect(result).to.equal(1.01);
+
+            result = utils.roundNumber(-24.700000000000003, -1);
+            expect(result).to.equal(-24.7);
+        });
+
+    });
+
 });
