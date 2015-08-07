da7d2c15a0350919afbbc1e2565e281015646636
Daniel Wippermann <daniel.wippermann@gmail.com>
2015-08-07 09:57:42 +0200

    Update VBus specification.

--- test/specs/specification-data.spec.js
+++ test/specs/specification-data.spec.js
@@ -6,7 +6,11 @@
 var _ = require('lodash');
 
 
-var Specification = require('./resol-vbus').Specification;
+var vbus = require('./resol-vbus');
+
+
+
+var Specification = vbus.Specification;
 
 
 
@@ -24,6 +28,37 @@ describe('SpecificationData', function() {
         _.forEach(specificationData.getRawValueFunctions, function(getRawValue) {
             getRawValue(buffer, 0, buffer.length);
         });
+    });
+
+    it('should calc multi-byte masks correctly', function() {
+    	var buffer = new Buffer(76);
+    	buffer.fill(0);
+
+    	var spec = Specification.getDefaultSpecification();
+
+    	var pfs = spec.getPacketFieldSpecification('00_0010_7761_0100_036_1_8388608');
+
+    	spec.setRawValue(pfs, 3, buffer);
+
+    	expect(buffer [36]).equal(0);
+    	expect(buffer [37]).equal(0);
+    	expect(buffer [38]).equal(128);
+    	expect(buffer [39]).equal(0);
+
+    	var rv = spec.getRawValue(pfs, buffer);
+
+    	expect(rv).equal(1);
+
+    	spec.setRawValue(pfs, 4, buffer);
+
+    	expect(buffer [36]).equal(0);
+    	expect(buffer [37]).equal(0);
+    	expect(buffer [38]).equal(0);
+    	expect(buffer [39]).equal(0);
+
+    	rv = spec.getRawValue(pfs, buffer);
+
+    	expect(rv).equal(0);
     });
 
 });
