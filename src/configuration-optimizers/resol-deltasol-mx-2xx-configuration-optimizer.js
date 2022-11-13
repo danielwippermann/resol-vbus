ee4a8139732ac4ba1fef257c89bb17d69e57babb
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2022-11-13 16:54:00 +0100

    Add WIP DeltaSol MX 2.x configuration optimizer.

---
+++ src/configuration-optimizers/resol-deltasol-mx-2xx-configuration-optimizer.js
@@ -0,0 +1,250 @@
+/*! resol-vbus | Copyright (c) 2013-2018, Daniel Wippermann | MIT license */
+'use strict';
+
+
+
+
+
+const configurationData = require('./resol-deltasol-mx-2xx-data');
+
+const BaseConfigurationOptimizer = require('../base-configuration-optimizer');
+const _ = require('../lodash');
+
+
+
+class ResolDeltaSolMx2xxConfigurationOptimizer extends BaseConfigurationOptimizer {
+
+    optimizeConfiguration($) {
+        this.optimizeAktorConfiguration($);
+        this.optimizeModuleConfiguration($);
+        this.optimizeSolarConfiguration($);
+        this.optimizeSolarWfConfiguration($);
+        this.optimizeAnlageWfConfiguration($);
+        this.optimizeHeizungWfConfiguration($);
+        this.optimizeHeizungHeizkreisConfiguration($);
+        this.optimizeHeizungRelaisConfiguration($);
+        this.optimizeWmzConfiguration($);
+        this.optimizeWsuConfiguration($);
+    }
+
+    optimizeAktorConfiguration($) {
+        $(/^(Aktor[0-9]+)_Funktion$/).forEach((value) => {
+            value.eql(0, () => {
+                $(`^${value.md [1]}_(?!Funktion).*`).ignore();
+            });
+        });
+    }
+
+    optimizeModuleConfiguration($) {
+        $(/^Modul([0-9]+)_Aktiviert$/).isFalse((value) => {
+            $('^(Sensor|Relais)[^_]*_Modul' + value.md [1] + '_.*$').ignore();
+        });
+    }
+
+    optimizeSolarConfiguration($) {
+        const value = $('Solar_SystemId');
+
+        value.isChanged(() => {
+            $(/^Solar_.*/).invalidate();
+            $(/^Anlage_.*/).invalidate();
+            $(/^Heizung_.*/).invalidate();
+        });
+
+        const hasMinKollektorCount = function (minCount) {
+            return function(solarSystemId) {
+                return (((solarSystemId / 10000) % 10) >= minCount);
+            }
+        };
+        const hasMinSpeicherCount = function (minCount) {
+            return function(solarSystemId) {
+                return (((solarSystemId / 100) % 10) >= minCount);
+            }
+        };
+
+        value.check(hasMinKollektorCount(1), () => {
+            $(/^Solar_Kol1_.*/).ignore();
+            $(/^Solar_Wf1_(Roehrenkollektor|Bypass|Frostschutz)_.*/).ignore();
+        });
+        value.check(hasMinKollektorCount(2), () => {
+            $(/^Solar_Kol2_.*/).ignore();
+            $(/^Solar_Wf2_(Roehrenkollektor|Bypass|Frostschutz)_.*/).ignore();
+        });
+        value.check(hasMinKollektorCount(3), () => {
+            $(/^Solar_Kol3_.*/).ignore();
+            $(/^Solar_Wf3_(Roehrenkollektor|Bypass|Frostschutz)_.*/).ignore();
+        });
+
+        value.check(hasMinSpeicherCount(1), () => {
+            $(/^Solar_Sp1_.*/).ignore();
+            $(/^Solar_Wf1_(ExtWT)_.*/).ignore();
+            $(/^Solar_Wf1_(Zieltemperatur|Bereitschaft|DrainBack)_.*/).ignore();
+        });
+        value.check(hasMinSpeicherCount(2), () => {
+            $(/^Solar_Sp2_.*/).ignore();
+            // TODO: Ladelogik
+            $(/^Solar_Wf2_(ExtWT)_.*/).ignore();
+        });
+        value.check(hasMinSpeicherCount(3), () => {
+            $(/^Solar_Sp3_.*/).ignore();
+            $(/^Solar_Wf3_(ExtWT)_.*/).ignore();
+        });
+        value.check(hasMinSpeicherCount(4), () => {
+            $(/^Solar_Sp4_.*/).ignore();
+            $(/^Solar_Wf4_(ExtWT)_.*/).ignore();
+        });
+        value.check(hasMinSpeicherCount(5), () => {
+            $(/^Solar_Sp5_.*/).ignore();
+            $(/^Solar_Wf5_(ExtWT)_.*/).ignore();
+        });
+    }
+
+    optimizeSolarWfConfiguration($) {
+        $(/^Solar_Wf([0-9]+)_Type$/).forEach((value) => {
+            const prefix = '^Solar_Wf' + value.md [1] + '_';
+
+            value.in([ 0, '#Frei' ], () => {
+                $(prefix + '(?!(Type|(Roehrenkollektor|Bypass|Frostschutz|ExtWT|Zieltemperatur|Bereitschaft|DrainBack)_)).*').ignore();
+            });
+
+            const wfTypes = [
+                'NhUnterdrueckung',
+                'Parallelrelais',
+                'Zwillingspumpe',
+                'Ueberwaerme',
+                'Volumenstromueberwachung',
+            ];
+
+            _.forEach(wfTypes, (wfType) => {
+                value.notEql('#' + wfType, () => {
+                    $(prefix + wfType + '_.*').ignore();
+                });
+            });
+        });
+    }
+
+    optimizeAnlageWfConfiguration($) {
+        $(/^(Anlage_Wf[0-9]+)_Type$/).forEach((value) => {
+            const prefix = '^' + value.md [1] + '_';
+
+            value.eql('#Frei', () => {
+                $(prefix + '(?!Type).*').ignore();
+            });
+
+            // $(prefix + 'OSchaltuhr').isFalse(() => {
+            //     $(prefix + 'Schaltuhr_.*').ignore();
+            // });
+
+            const wfTypes = [
+                'Parallelrelais',
+                'Mischer',
+                'Boilerladung',
+                'Waermeaustausch',
+                'Feststoffkessel',
+                // 'Zirkulation',
+                'Ruecklaufanhebung',
+                'Funktionsblock',
+                'Strahlungsschalter',
+            ];
+
+            _.forEach(wfTypes, (wfType) => {
+                value.notEql('#' + wfType, () => {
+                    $(prefix + wfType + '_.*').ignore();
+                });
+            });
+        });
+    }
+
+    optimizeHeizungWfConfiguration($) {
+        $(/^(Heizung_Wf[0-9]+)_Type$/).forEach((value) => {
+            const prefix = '^' + value.md [1] + '_';
+
+            value.eql('#Frei', () => {
+                $(prefix + '(?!Type).*').ignore();
+            });
+
+            // $(prefix + 'OSchaltuhr').isFalse(() => {
+            //     $(prefix + 'Schaltuhr_.*').ignore();
+            // });
+
+            const wfTypes = [
+                'ThDesinfektion',
+                'ThBwErwaermung',
+            ];
+
+            _.forEach(wfTypes, (wfType) => {
+                value.notEql('#' + wfType, () => {
+                    $(prefix + wfType + '_.*').ignore();
+                });
+            });
+        });
+    }
+
+    optimizeHeizungHeizkreisConfiguration($) {
+        $(/^(Heizung_Heizkreis[0-9]+)_Type$/).forEach((value) => {
+            const prefix = '^' + value.md [1] + '_';
+
+            value.eql('#Frei', () => {
+                $(prefix + '(?!Type).*').ignore();
+            });
+
+            // $(prefix + 'OSchaltuhr').isFalse(() => {
+            //     $(prefix + 'Schaltuhr_.*').ignore();
+            // });
+
+            $(prefix + '(Raumthermostat[0-9]+)_Typ$').forEach((value) => {
+                const rthPrefix = prefix + value.md [1] + '_';
+
+                value.eql(0, () => {
+                    $(rthPrefix + '(?!Typ).*').ignore();
+                });
+
+                $(rthPrefix + 'OSchaltuhr').isFalse(() => {
+                    $(rthPrefix + 'Schaltuhr_.*').ignore();
+                });
+            });
+        });
+    }
+
+    optimizeHeizungRelaisConfiguration($) {
+        $(/^(Heizung_Relais(Anforderung|Pumpe|Ventil)[0-9]+)_Type$/).forEach((value) => {
+            const prefix = `^${value.md [1]}_`;
+
+            value.eql(0, () => {
+                $(prefix + '(?!Type).*').ignore();
+            });
+        });
+    }
+
+    optimizeWmzConfiguration($) {
+        $(/^(Wmz[0-9]+)_Type$/).forEach((value) => {
+            const prefix = '^' + value.md [1] + '_';
+
+            value.eql(0, () => {
+                $(prefix + '(?!Type).*').ignore();
+            });
+        });
+    }
+
+    optimizeWsuConfiguration($) {
+        $(/^(Wsu[0-9]+)_Belegt$/).forEach((value) => {
+            const prefix = `^${value.md [1]}_`;
+            value.eql(0, () => {
+                $(prefix + '(?!Belegt).*').ignore();
+            });
+        });
+    }
+
+}
+
+
+Object.assign(ResolDeltaSolMx2xxConfigurationOptimizer, /** @lends ResolDeltaSolMx2xxConfigurationOptimizer */ {
+
+    deviceAddress: 0x7E11,
+
+    configurationData,
+
+});
+
+
+
+module.exports = ResolDeltaSolMx2xxConfigurationOptimizer;
---
+++ test/specs/configuration-optimizers/resol-deltasol-mx-2xx-configuration-optimizer.spec.js
@@ -0,0 +1,117 @@
+/*! resol-vbus | Copyright (c) 2013-2018, Daniel Wippermann | MIT license */
+'use strict';
+
+
+
+const {
+    BaseConfigurationOptimizer,
+    ConfigurationOptimizerFactory,
+} = require('../resol-vbus');
+
+
+const jestExpect = global.expect;
+const expect = require('../expect');
+const _ = require('../lodash');
+const testUtils = require('../test-utils');
+const ResolDeltaSolMx2xxConfigurationOptimizer = require('../../../src/configuration-optimizers/resol-deltasol-mx-112-configuration-optimizer');
+
+
+const { wrapAsPromise } = testUtils;
+
+
+
+const optimizerPromise = ConfigurationOptimizerFactory.createOptimizerByDeviceAddress(0x7E11);
+
+
+
+describe('ResolDeltaSolMx2xxConfigurationOptimizer', () => {
+
+    describe('using ConfigurationOptimizerFactory', () => {
+
+        it('should work correctly', () => {
+            return testUtils.expectPromise(optimizerPromise).then((optimizer) => {
+                expect(optimizer).an('object');
+            });
+        });
+
+    });
+
+    describe('#completeConfiguration', () => {
+
+        it('should work correctly', () => {
+            return optimizerPromise.then((optimizer) => {
+                return wrapAsPromise(() => {
+                    return testUtils.expectPromise(optimizer.completeConfiguration());
+                }).then((config) => {
+                    expect(config).an('array').lengthOf(6144);
+                });
+            });
+        });
+
+    });
+
+    describe('#optimizeLoadConfiguration', () => {
+
+        it('should work correctly after', () => {
+            return optimizerPromise.then((optimizer) => {
+                return wrapAsPromise(() => {
+                    return testUtils.expectPromise(optimizer.completeConfiguration());
+                }).then((config) => {
+                    return testUtils.expectPromise(optimizer.optimizeLoadConfiguration(config));
+                }).then((config) => {
+                    expect(config).an('array');
+
+                    const valueIds = _.reduce(config, (memo, value) => {
+                        if (value.pending) {
+                            memo.push(value.valueId);
+                        }
+                        return memo;
+                    }, []);
+
+                    expect(valueIds).lengthOf(552);
+
+                    _.forEach(config, (value) => {
+                        if (value.pending) {
+                            value.pending = false;
+                            value.transceived = true;
+                            value.value = null;
+                        }
+                    });
+
+                    return testUtils.expectPromise(optimizer.optimizeLoadConfiguration(config));
+                }).then((config) => {
+                    expect(config).an('array');
+
+                    const valueIds = _.reduce(config, (memo, value) => {
+                        if (value.pending) {
+                            memo.push(value.valueId);
+                        }
+                        return memo;
+                    }, []);
+
+                    expect(valueIds).lengthOf(33);
+                });
+            });
+        });
+
+    });
+
+    testUtils.itShouldWorkCorrectlyAfterMigratingToClass(ResolDeltaSolMx2xxConfigurationOptimizer, BaseConfigurationOptimizer, {
+        constructor: Function,
+        optimizeConfiguration: Function,
+        optimizeAktorConfiguration: Function,
+        optimizeModuleConfiguration: Function,
+        optimizeSolarConfiguration: Function,
+        optimizeSolarWfConfiguration: Function,
+        optimizeAnlageWfConfiguration: Function,
+        optimizeHeizungWfConfiguration: Function,
+        optimizeHeizungHeizkreisConfiguration: Function,
+        optimizeHeizungRelaisConfiguration: Function,
+        optimizeWmzConfiguration: Function,
+        optimizeWsuConfiguration: Function,
+    }, {
+        deviceAddress: 0x7E11,
+        configurationData: jestExpect.any(Object),
+    });
+
+});
